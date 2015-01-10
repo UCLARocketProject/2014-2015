225a462d6bf95ec0610f8fee25beaaa05f9750c5
KyCodeHuynh <KyCodeHuynh@gmail.com>
2015-01-09 22:20:43 -0800

    Code for acquiring temperature sensor data. Code for writing to SD card via Arduino. Integration of the two remains to be done.

--- Calibration/tempToSDCard/tempToSDCard.ino
+++ Calibration/tempToSDCard/tempToSDCard.ino
@@ -1,9 +1,8 @@
 /*
   SD card datalogger
-
- This example shows how to log data from three analog sensors
- to an SD card using the SD library.
-
+ 
+Log data from temperature sensor to SD card. 
+ 
  The circuit:
  * analog sensors on analog ins 0, 1, and 2
  * SD card attached to SPI bus as follows:
@@ -11,23 +10,25 @@
  ** MISO - pin 12
  ** CLK - pin 13
  ** CS - pin 4
-
+ 
  created  24 Nov 2010
  modified 9 Apr 2012
  by Tom Igoe
-
+ 
  This example code is in the public domain.
-
+ 
  */
 
 #include <SPI.h>
 #include <SD.h>
 
-// On the Ethernet Shield, CS is pin 4. Note that even if it's not
+// Note the chip select (CS) pin. Note that even if it's not
 // used as the CS pin, the hardware CS pin (10 on most Arduino boards,
 // 53 on the Mega) must be left as an output or the SD library
 // functions will not work.
-const int chipSelect = 4;
+const int chipSelect = 10;
+
+int failed = 0; 
 
 void setup()
 {
@@ -44,6 +45,7 @@ void setup()
   if (!SD.begin(chipSelect)) {
     Serial.println("Card failed, or not present");
     // don't do anything more:
+    failed = 1;
     return;
   }
   Serial.println("Card initialized.");
@@ -51,20 +53,23 @@ void setup()
 
 void loop()
 {
+  if (failed == 1) {
+    return;
+  }
   // make a string for assembling the data to log:
   String dataString = "";
 
   // Temperature sensor at A0
-  int analogPin = 0;
-  
+  int digitalPin = 11;
+
   int numValues = 100;
   for (int i = 0; i < numValues; i++) {
     // Read raw value, which is 0-1023
-    double sensorValue = (double)analogRead(analogPin);
-    
+    double sensorValue = (double)digitalRead(digitalPin);
+
     // Compute as fraction of voltage against 5V max
     double voltage = (sensorValue / 1023) * 5;
-    
+
     // Convert:        Temperature = 100 * voltage - 50
     // Equation form:  T = 100 * V - 50
     double temp = 100 * voltage - 50;
@@ -74,7 +79,7 @@ void loop()
 
   // open the file. note that only one file can be open at a time,
   // so you have to close this one before opening another.
-  File dataFile = SD.open("datalog.txt", FILE_WRITE);
+  File dataFile = SD.open("TEMP001.TXT", FILE_WRITE);
 
   // if the file is available, write to it:
   if (dataFile) {
@@ -90,3 +95,4 @@ void loop()
     return;
   }
 }
+
