a1426647bb84537ea9ca4c835c415687860c3580
KyCodeHuynh <KyCodeHuynh@gmail.com>
2015-01-16 18:17:27 -0800

    Combined temperature-reading and SD-writing code.

--- Calibration/tempToSDCard/tempToSDCard.ino
+++ Calibration/tempToSDCard/tempToSDCard.ino
@@ -1,61 +1,31 @@
-/*
-  SD card datalogger
- 
-Log data from temperature sensor to SD card. 
- 
- The circuit:
- * analog sensors on analog ins 0, 1, and 2
- * SD card attached to SPI bus as follows:
- ** MOSI - pin 11
- ** MISO - pin 12
- ** CLK - pin 13
- ** CS - pin 4
- 
- created  24 Nov 2010
- modified 9 Apr 2012
- by Tom Igoe
- 
- This example code is in the public domain.
- 
- */
-
-#include <SPI.h>
 #include <SD.h>
+#include <SPI.h>
 
-// Note the chip select (CS) pin. Note that even if it's not
-// used as the CS pin, the hardware CS pin (10 on most Arduino boards,
-// 53 on the Mega) must be left as an output or the SD library
-// functions will not work.
-const int chipSelect = 10;
-
-int failed = 0; 
-
-void setup()
+void setup() 
 {
-  // Open serial communications and wait for port to open:
+  // This is needed for the duemilanove and uno without ethernet shield
+  const int sdCardPin = 10;
+  int loopCount = 0;
+
+  // TODO: Serial output for testing only. 
   Serial.begin(9600);
   delay(1000);
 
-  Serial.print("Initializing SD card...");
-  // make sure that the default chip select pin is set to
-  // output, even if you don't use it:
+  // Pin 10 needs to be initialized as output
+  // for SD operations to work
   pinMode(10, OUTPUT);
-
-  // see if the card is present and can be initialized:
-  if (!SD.begin(chipSelect)) {
+  
+  if (!SD.begin(sdCardPin)) {
     Serial.println("Card failed, or not present");
     // don't do anything more:
-    failed = 1;
-    return;
+    return; 
   }
+
   Serial.println("Card initialized.");
 }
 
-void loop()
+void loop() 
 {
-  if (failed == 1) {
-    return;
-  }
   // make a string for assembling the data to log:
   String dataString = "";
 
@@ -77,22 +47,27 @@ void loop()
     dataString += "\n";
   }
 
-  // open the file. note that only one file can be open at a time,
-  // so you have to close this one before opening another.
-  File dataFile = SD.open("TEMP001.TXT", FILE_WRITE);
+  sdWrite("dataString");
+}
 
-  // if the file is available, write to it:
+/*
+Filename MUST be <=8 characters (not including the file extension) or the
+file will not be created
+*/
+bool sdWrite(String data) 
+{
+  File dataFile = SD.open("temp.txt", FILE_WRITE);
+  // If the file is available, write to it:
   if (dataFile) {
-    dataFile.println(dataString);
+    dataFile.println(data);
     dataFile.close();
-    // print to the serial port too:
-    Serial.println(dataString);
-    return;
+    // Print to the serial port too:
+    Serial.println(data);
   }
-  // if the file isn't open, pop up an error:
+  // If the file isn't open, pop up an error:
   else {
-    Serial.println("[ERROR]: opening datalog.txt");
-    return;
+    Serial.println("Error opening file");
+    dataFile.close();
   }
 }
 
