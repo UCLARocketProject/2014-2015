4b9981b79bb573b39786f76b0e19315796c84a34
KyCodeHuynh <KyCodeHuynh@gmail.com>
2015-01-16 20:00:52 -0800

    Rename files and folders to match current configuration.

---
+++ Calibration/oldTempData/tempToSDCard.ino
@@ -0,0 +1,79 @@
+#include <SD.h>
+#include <SPI.h>
+
+void setup() 
+{
+  // This is needed for the duemilanove and uno without ethernet shield
+  const int sdCardPin = 10;
+  int loopCount = 0;
+  
+  // Temperature sensor at A0
+  int digitalPin = 11;
+
+  // TODO: Serial output for testing only. 
+  Serial.begin(9600);
+  delay(1000);
+
+  // Pin 10 needs to be initialized as output
+  // for SD operations to work
+  pinMode(10, OUTPUT);
+  
+  if (!SD.begin(sdCardPin)) {
+    Serial.println("Card failed, or not present");
+    // don't do anything more:
+    return; 
+  }
+
+  Serial.println("Card initialized.");
+}
+
+void loop() 
+{
+  // make a string for assembling the data to log:
+  String dataString = "";
+
+  // Temperature sensor at A0
+  int digitalPin = 11;
+
+  int numValues = 100;
+  for (int i = 0; i < numValues; i++) {
+    // Read raw value, which is 0-1023
+    double sensorValue = (double)digitalRead(digitalPin);
+    
+    // Compute as fraction of voltage against 5V max
+    double voltage = (sensorValue / 1023) * 5;
+
+    // Convert:        Temperature = 100 * voltage - 50
+    // Equation form:  T = 100 * V - 50
+    double temp = 100 * voltage - 50;
+    Serial.println(temp);
+    dataString += String(temp);
+    dataString += "Degrees Celsius";
+    dataString += "\n";
+  }
+
+  sdWrite(dataString);
+  return;
+}
+
+/*
+Filename MUST be <=8 characters (not including 
+the file extension) or the file will not be created
+*/
+bool sdWrite(String data) 
+{
+  File dataFile = SD.open("temp.txt", FILE_WRITE);
+  // If the file is available, write to it:
+  if (dataFile) {
+    dataFile.println(data);
+    dataFile.close();
+    // Print to the serial port too:
+    Serial.println(data);
+  }
+  // If the file isn't open, pop up an error:
+  else {
+    Serial.println("Error opening file");
+    dataFile.close();
+  }
+}
+
--- Calibration/tempToSDCard/tempToSDCard.ino
+++ Calibration/tempToSDCard/tempToSDCard.ino
@@ -1,79 +1,93 @@
+/* Temperature to SD Card 
+ * UCLA Rocket Project
+ * 16 January 2015
+ */
+
 #include <SD.h>
 #include <SPI.h>
 
+// Number of desired data points
+const int numValues = 100;
+// This is needed for the Duemilanove 
+// and Uno without ethernet shield
+const int SDCardPin = 10;
+// Analog 0
+const int tempPin = A0;
+// Range of TMP36
+const int TMP36_range = 1023;
+// Maximum voltage response of TMP36
+const int TMP36_maxVolt = 5;
+
 void setup() 
 {
-  // This is needed for the duemilanove and uno without ethernet shield
-  const int sdCardPin = 10;
-  int loopCount = 0;
+    // TODO: For testing only 
+    Serial.begin(9600);
+    delay(1000);
+    pinMode(10, OUTPUT);
   
-  // Temperature sensor at A0
-  int digitalPin = 11;
+    if (!SD.begin(SDCardPin)) {
+        Serial.println("Card failed, or not present");
+        // Don't do anything more:
+        return; 
+    }
 
-  // TODO: Serial output for testing only. 
-  Serial.begin(9600);
-  delay(1000);
-
-  // Pin 10 needs to be initialized as output
-  // for SD operations to work
-  pinMode(10, OUTPUT);
-  
-  if (!SD.begin(sdCardPin)) {
-    Serial.println("Card failed, or not present");
-    // don't do anything more:
-    return; 
-  }
-
-  Serial.println("Card initialized.");
+    Serial.println("Card initialized.");
 }
 
 void loop() 
 {
-  // make a string for assembling the data to log:
-  String dataString = "";
+    String data;
+    for (int i = 0; i < numValues; i++) {
+        double temp 
+            = temperature(TMP36_range, tempPin, TMP36_maxVolt);
+        data += String(temp);
+        data += '\n';
+    }
 
-  // Temperature sensor at A0
-  int digitalPin = 11;
-
-  int numValues = 100;
-  for (int i = 0; i < numValues; i++) {
-    // Read raw value, which is 0-1023
-    double sensorValue = (double)digitalRead(digitalPin);
-    
-    // Compute as fraction of voltage against 5V max
-    double voltage = (sensorValue / 1023) * 5;
-
-    // Convert:        Temperature = 100 * voltage - 50
-    // Equation form:  T = 100 * V - 50
-    double temp = 100 * voltage - 50;
-    Serial.println(temp);
-    dataString += String(temp);
-    dataString += "Degrees Celsius";
-    dataString += "\n";
-  }
-
-  sdWrite(dataString);
-  return;
+    SD_write(data);
 }
 
 /*
-Filename MUST be <=8 characters (not including 
-the file extension) or the file will not be created
-*/
-bool sdWrite(String data) 
+ * Filename MUST be <=8 characters (not including the 
+ * file extension) or the file will not be created
+ */
+bool SD_write(String data) 
 {
-  File dataFile = SD.open("temp.txt", FILE_WRITE);
-  // If the file is available, write to it:
-  if (dataFile) {
-    dataFile.println(data);
-    dataFile.close();
-    // Print to the serial port too:
-    Serial.println(data);
-  }
+    File dataFile = SD.open("temp.txt", FILE_WRITE);
+    // If the file is available, write to it:
+    if (dataFile) {
+        dataFile.println(data);
+        dataFile.close();
+        // Print to the serial port too:
+        Serial.println(data);
+      }
   // If the file isn't open, pop up an error:
-  else {
-    Serial.println("Error opening file");
-    dataFile.close();
-  }
+    else {
+        Serial.println("Error opening file");
+        dataFile.close();
+    }
+}
+
+/* TMP36 conversion from voltage to temperature in Celsius */
+double voltToTemp(int voltage)
+{
+    return 100 * voltage - 50;
+}
+
+/* sensorRange is the range of values returned by 
+ * the sensor, e.g. a temperature sensor with 1024 levels, 
+ * spanning 0 to 1023. 
+ * pinNum is the digital pin number. 
+ * voltMax is the maximum voltage response of the sensor
+ * Returns Temperature reading in Celsius
+ */
+double temperature(int sensorRange, int pinNum, unsigned voltMax)
+{
+        // Read raw value, which is 0-sensorRange
+        double sensorValue = (double)analogRead(pinNum);
+        // Compute as fraction of voltage against voltage max
+        double voltage = (sensorValue / sensorRange) * voltMax;
+        Serial.println(voltToTemp(voltage));
+        return voltToTemp(voltage);
 }
 
