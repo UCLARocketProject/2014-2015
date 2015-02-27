cdc775ec7faac31ea5aeaf49003dd9644704c27b
Alex Omar <omar112694@gmail.com>
2015-02-26 21:54:34 -0800

    Added a live code folder for the code thats running on the arduino. removed the old folder

--- Calibration/accel/accel.ino
+++
@@ -1,7 +0,0 @@
-#include <Wire.h>
-#include <Adafruit_Sensor.h>
-#include <Adafruit_LSM303_U.h>
-
-// Construct the un
-Adafruit_10DOF                dof   = Adafruit_10DOF();
-Adafruit_LSM303_Accel_Unified accel = Adafruit_LSM303_Accel_Unified(30301);
