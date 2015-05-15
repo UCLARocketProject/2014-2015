789358444e6f1f9c8de4f287224cf801aee27808
Alex <omar112694@gmail.com>
2015-05-14 19:24:04 -0700

    removing code and sme of the redundant safeties we placed in order to increase write speed.

--- Calibration/liveCode/overclockAccel/overclockAccel.ino
+++ Calibration/liveCode/overclockAccel/overclockAccel.ino
@@ -87,73 +87,9 @@ bool sdWriteNewline() {
   }
 }
 
-bool sdWrite(String data) {
+void sdWrite(String data) {
   File dataFile = SD.open(FILENAME, FILE_WRITE);
   // if the file is available, write to it:
-  if (dataFile) {
-    dataFile.print(data);
-    dataFile.print(", ");
-    dataFile.close();
-    // print to the serial port too:
-  }
-  // if the file isn't open, pop up an error:
-  else {
-    dataFile.close();
-  }
-}
-char * floatToString(char * outstr, double val, byte precision, byte widthp){
- char temp[16]; //increase this if you need more digits than 15
- byte i;
-
- temp[0]='\0';
- outstr[0]='\0';
-
- if(val < 0.0){
-   strcpy(outstr,"-\0");  //print "-" sign
-   val *= -1;
- }
-
- if( precision == 0) {
-   strcat(outstr, ltoa(round(val),temp,10));  //prints the int part
- }
- else {
-   unsigned long frac, mult = 1;
-   byte padding = precision-1;
-   
-   while (precision--)
-     mult *= 10;
-
-   val += 0.5/(float)mult;      // compute rounding factor
-   
-   strcat(outstr, ltoa(floor(val),temp,10));  //prints the integer part without rounding
-   strcat(outstr, ".\0"); // print the decimal point
-
-   frac = (val - floor(val)) * mult;
-
-   unsigned long frac1 = frac;
-
-   while(frac1 /= 10) 
-     padding--;
-
-   while(padding--) 
-     strcat(outstr,"0\0");    // print padding zeros
-
-   strcat(outstr,ltoa(frac,temp,10));  // print fraction part
- }
-
- // generate width space padding 
- if ((widthp != 0)&&(widthp >= strlen(outstr))){
-   byte J=0;
-   J = widthp - strlen(outstr);
-
-   for (i=0; i< J; i++) {
-     temp[i] = ' ';
-   }
-
-   temp[i++] = '\0';
-   strcat(temp,outstr);
-   strcpy(outstr,temp);
- }
-
- return outstr;
+  dataFile.print(data + ", ");
+  dataFile.close();
 }
